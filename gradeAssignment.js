2984158fe4c47c49aa5b42cc00844b45d9a57b8d
Villanueva, Robert G. <bertvillxxxxva@gmail.com>
2024-10-03 06:54:04 -0800

    Add files via upload

---
+++ gradeAssignment.js
@@ -0,0 +1,13 @@
+let score = 100;
+let enterYourScore = score
+
+console.log("Enter Your Score:", enterYourScore);
+
+if(score >= 90 && score <= 100)
+{console.log("Your grade is: Excellent.");}
+else if(score >= 80 && score <= 89)
+{console.log("Your grade is: Good.");}
+else if(score >= 70 && score <= 79)
+{console.log("Your grade is: Fair.")}
+else
+{console.log("Your grade is: Needs Improvement.")}
